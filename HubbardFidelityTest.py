d60e60c0d88783700c573517828904f67a9d032d
Elandle <jwwalker@ucdavis.edu>
2022-07-14 09:37:17 -0700

    Added Hubbard fidelity test
    Added a code for quickly checking the fidelity of a set of parameters for the Hubbard model.

---
+++ HubbardFidelityTest.py
@@ -0,0 +1,99 @@
+import matplotlib.pyplot
+import numpy
+import itertools
+import copy
+import random
+import scipy.linalg
+import scipy.optimize
+import time 
+
+# Variables deciding what we are trying to find the fidelity for
+# N: amount of sites
+# u: amount of up electrons
+# d: amount of down electrons
+# starting: where the electrons begin at (using [up, up, ..., down, down, ...] notation; not Fock) starting from site 0 to site N-1
+# ending: where to (hopefully) find the electrons to be at; using the same notation as starting
+# t: coupling values (N- 1 of them)
+# time: time to evaluate the fidelity at 
+# e: interaction energy (usually denoted as u, but here it is e)
+N= 6
+u= 2
+d= 2
+starting= [0, 1, 0, 1]
+ending= [N- 1, N- 2, N- 1, N- 2]
+t= [1, 2, 3, 4, 5]
+time= numpy.pi/2
+e= 1
+
+
+# Functions used later for basic setting up
+# Generates states and basis
+# ---------------------------------------------------------------------------------------
+def repeat(x, i, j):
+    setx= set(x[i: j])
+    return len(x[i: j])!= len(setx)
+def permuted(x, y):
+    upxset= set(x[:u])
+    upyset= set(y[:u])
+    downxset= set(x[u: u+ d])
+    downyset= set(y[u: u+ d])
+    return upxset==upyset and downxset==downyset
+def sortstate(s):
+    ss= copy.copy(s)
+    sortups= ss[:u]
+    sortups.sort()
+    sortdowns= ss[u: u+ d]
+    sortdowns.sort()
+    return sortups+ sortdowns
+def makestates():
+    x= [i for i in range(N)]
+    z= itertools.product(x, repeat= u+ d)
+    z= list(z)
+    for a in z[:]:
+        if(repeat(a, 0, u) or repeat(a, u, u+ d)): z.remove(a)
+    dummyz= z        
+    for a in dummyz:
+        for b in dummyz:
+            if((a!=b) and permuted(a, b)):
+                z.remove(b)
+    states= [list(a) for a in z]
+    sorting= [sortstate(a) for a in states]
+    sorting.sort()
+    states= sorting
+    return states
+def makebasis(states):
+    basis= []
+    for a in states:
+        x= [0 for i in range(2*N)]
+        for i in range(u):
+            x[a[i]]= x[a[i]]+ 1
+        for i in range(d):
+            x[N+ a[u+ i]]= x[N+ a[u+ i]]+ 1
+        basis.append(x)
+    return basis
+def sindex(s):
+    ss= copy.copy(s)
+    ss= sortstate(ss)
+    return states.index(ss)
+# ---------------------------------------------------------------------------------------
+
+
+# function for calculating fidelity
+def fid(t, time, e):
+    hamil= hammer(t, e)
+    return (numpy.abs(scipy.linalg.expm(complex(0, -time)*hamil)@initial)**2)[state]
+# ---------------------------------------------------------------------------------------
+
+# Making the states and basis list, also initializing the states we are evolving from (initial) and trying to go to (using the state number of this)
+states= makestates()
+slength= len(states)
+basis= makebasis(states)
+initial= numpy.zeros(slength)
+initial[sindex(starting)]= 1
+state= sindex(ending) 
+
+
+# print the fidelity
+print(f"Fidelity= {fid(t, time, e)}")
+
+
